08cf91962237cef789b8de71beb1f708bd446e7c
Ude.solraK <keduardo.lacerda@gmail.com>
2024-12-01 22:10:21 -0300

    deu certo

---
+++ Projeto PW/gerenciamento_fornecedores.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 02/12/2024 às 02:08
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `gerenciamento_fornecedores`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fornecedores`
+--
+
+CREATE TABLE `fornecedores` (
+  `id` int(11) NOT NULL,
+  `nome_forn` varchar(256) NOT NULL,
+  `cnpj_forn` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `fornecedores`
+--
+
+INSERT INTO `fornecedores` (`id`, `nome_forn`, `cnpj_forn`) VALUES
+(17, 'nagumo', '12345678912345'),
+(18, 'assai', '12345678912345'),
+(19, 'atacadao', '12345678912345'),
+(20, 'walmart', '12345678912345'),
+(21, 'walmart', '12345678912345'),
+(22, 'nagumo', '12345678912345'),
+(23, 'aksjdhkasdlas', 'sdjiashdkasjnas'),
+(24, '', ''),
+(25, '', ''),
+(26, 'xcz', 'zxczx'),
+(27, 'dsdas', 'dasdas'),
+(28, 'sdad', 'asdsd');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `fornecedores`
+--
+ALTER TABLE `fornecedores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `fornecedores`
+--
+ALTER TABLE `fornecedores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
